8050e91148366d49dd1b6adcee190bebeda9ca7c
juli-ro <julianroessler@gmx.de>
2026-05-25 12:01:43 +0200

    completed initial rework of meet functionality

--- src/main/java/de/gametogather/gtgspringbackend/controller/MeetController.java
+++ src/main/java/de/gametogather/gtgspringbackend/controller/MeetController.java
@@ -1,6 +1,8 @@
 package de.gametogather.gtgspringbackend.controller;
 
+import de.gametogather.gtgspringbackend.model.dto.MeetDateSuggestionDto;
 import de.gametogather.gtgspringbackend.model.dto.MeetDto;
+import de.gametogather.gtgspringbackend.model.dto.MeetUserDto;
 import de.gametogather.gtgspringbackend.service.MeetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -42,7 +44,7 @@ public class MeetController {
         try {
             meetService.deleteMeet(meetId);
             return ResponseEntity.noContent().build();
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return ResponseEntity.notFound().build();
         }
     }
@@ -55,7 +57,7 @@ public class MeetController {
 
     //Todo: rename url in frontend
     @GetMapping("/user-meets")
-    public List<MeetDto> getAllUserMeets(@AuthenticationPrincipal Jwt jwt){
+    public List<MeetDto> getAllUserMeets(@AuthenticationPrincipal Jwt jwt) {
         UUID userId = UUID.fromString(jwt.getSubject());
         return meetService.getAllUserMeets(userId);
     }
@@ -70,5 +72,49 @@ public class MeetController {
     public ResponseEntity<UUID> createGroupMeet(@PathVariable UUID groupId) {
         UUID newId = meetService.createGroupMeet(groupId);
         return ResponseEntity.status(HttpStatus.CREATED).body(newId);
+    }
+
+    //Todo: rename url in frontend
+    @PostMapping("add-date")
+    public ResponseEntity<MeetDateSuggestionDto> addDateSuggestion(@RequestBody MeetDateSuggestionDto meetDateSuggestionDto) {
+        try {
+            MeetDateSuggestionDto saveSuggestion = meetService.addDate(meetDateSuggestionDto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(saveSuggestion);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    //Todo: rename url in frontend
+    @PutMapping("select-date/{meetDateId}")
+    public ResponseEntity<MeetDateSuggestionDto> selectDate(@PathVariable UUID meetDateId) {
+        try {
+            MeetDateSuggestionDto result = meetService.selectDate(meetDateId);
+            return ResponseEntity.ok(result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    //Todo: rename url in frontend
+    @GetMapping("current-meet-user/{meetId}")
+    public ResponseEntity<MeetUserDto> getCurrentMeetUser(@PathVariable UUID meetId, @AuthenticationPrincipal Jwt jwt) {
+        UUID userId = UUID.fromString(jwt.getSubject());
+
+        return meetService.getCurrentMeetUser(meetId, userId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //Todo: rename url in frontend
+    @PatchMapping("/participant/{meetUserId}")
+    public ResponseEntity<Boolean> updateParticipant(@PathVariable UUID meetUserId, @RequestBody boolean isParticipating) {
+        try {
+            boolean result = meetService.updateParticipant(meetUserId, isParticipating);
+
+            return ResponseEntity.ok(result);
+        }  catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
--- src/main/java/de/gametogather/gtgspringbackend/service/MeetService.java
+++ src/main/java/de/gametogather/gtgspringbackend/service/MeetService.java
@@ -1,12 +1,17 @@
 package de.gametogather.gtgspringbackend.service;
 
+import de.gametogather.gtgspringbackend.mapper.MeetDateSuggestionMapper;
 import de.gametogather.gtgspringbackend.mapper.MeetMapper;
-import de.gametogather.gtgspringbackend.model.dto.GroupDto;
+import de.gametogather.gtgspringbackend.mapper.MeetUserMapper;
+import de.gametogather.gtgspringbackend.model.dto.MeetDateSuggestionDto;
 import de.gametogather.gtgspringbackend.model.dto.MeetDto;
+import de.gametogather.gtgspringbackend.model.dto.MeetUserDto;
 import de.gametogather.gtgspringbackend.model.entity.Group;
 import de.gametogather.gtgspringbackend.model.entity.Meet;
+import de.gametogather.gtgspringbackend.model.entity.MeetDateSuggestion;
 import de.gametogather.gtgspringbackend.model.entity.MeetUser;
 import de.gametogather.gtgspringbackend.repository.GroupRepository;
+import de.gametogather.gtgspringbackend.repository.MeetDateSuggestionRepository;
 import de.gametogather.gtgspringbackend.repository.MeetRepository;
 import de.gametogather.gtgspringbackend.repository.MeetUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +28,10 @@ public class MeetService {
     private final MeetRepository meetRepository;
     private final GroupRepository groupRepository;
     private final MeetUserRepository meetUserRepository;
+    private final MeetDateSuggestionRepository meetDateSuggestionRepository;
     private final MeetMapper meetMapper;
+    private final MeetDateSuggestionMapper meetDateSuggestionMapper;
+    private final MeetUserMapper meetUserMapper;
 
     public List<MeetDto> getAllMeets() {
         return meetRepository.findAll().stream()
@@ -35,11 +43,12 @@ public class MeetService {
         return meetRepository.findByIdWithDetails(id).map(meetMapper::toDto);
     }
 
-    public MeetDto createMeet(MeetDto meetDto) {
-        Meet meet = meetMapper.toEntity(meetDto);
-        Meet updatedMeet = meetRepository.save(meet);
-        return meetMapper.toDto(updatedMeet);
-    }
+    //Todo: every meet is probably a group meet so this might be obsolete (see controller)
+//    public MeetDto createMeet(MeetDto meetDto) {
+//        Meet meet = meetMapper.toEntity(meetDto);
+//        Meet updatedMeet = meetRepository.save(meet);
+//        return meetMapper.toDto(updatedMeet);
+//    }
 
     public void deleteMeet(UUID id) {
         meetRepository.deleteById(id);
@@ -76,12 +85,50 @@ public class MeetService {
 
         Meet updatedMeet = meetRepository.save(newMeet);
 
-        for (var groupUser : meetGroup.getGroupUsers()){
+        for (var groupUser : meetGroup.getGroupUsers()) {
             meetUserRepository.save(MeetUser.builder()
                     .meet(newMeet)
                     .user(groupUser.getUser())
                     .build());
         }
         return updatedMeet.getId();
+    }
+
+    public MeetDateSuggestionDto addDate(MeetDateSuggestionDto meetDateSuggestionDto) {
+
+        if (meetDateSuggestionRepository.existsByMeet_IdAndDate(meetDateSuggestionDto.meetId(), meetDateSuggestionDto.date())) {
+            throw new IllegalArgumentException("Date already exists");
+        }
+
+        MeetDateSuggestion meetDateSuggestion = meetDateSuggestionMapper.toEntity(meetDateSuggestionDto);
+
+        Meet meet = meetRepository.getReferenceById(meetDateSuggestionDto.meetId());
+        meetDateSuggestion.setMeet(meet);
+        meetDateSuggestionRepository.save(meetDateSuggestion);
+
+        return meetDateSuggestionMapper.toDto(meetDateSuggestion);
+    }
+
+
+    public MeetDateSuggestionDto selectDate(UUID meetDateId) {
+        MeetDateSuggestion meetDateSuggestion = meetDateSuggestionRepository.findById(meetDateId)
+                .orElseThrow(() -> new IllegalArgumentException("No such meet id " + meetDateId));
+        meetDateSuggestion.setIsChosenDate(!meetDateSuggestion.getIsChosenDate());
+        meetDateSuggestionRepository.save(meetDateSuggestion);
+        return meetDateSuggestionMapper.toDto(meetDateSuggestion);
+    }
+
+    public Optional<MeetUserDto> getCurrentMeetUser(UUID meetId, UUID userId) {
+        return meetUserRepository.findByMeet_IdAndUser_Id(meetId, userId)
+                .map(meetUserMapper::toDto);
+    }
+
+    public Boolean updateParticipant(UUID meetUserId, boolean isParticipating) {
+        MeetUser meetUser = meetUserRepository.findById(meetUserId).orElseThrow(() -> new IllegalArgumentException("No such meet user " + meetUserId));
+
+        meetUser.setIsParticipating(isParticipating);
+        meetUserRepository.save(meetUser);
+
+        return isParticipating;
     }
 }
